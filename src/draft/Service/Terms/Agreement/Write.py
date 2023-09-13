13bbb5cf26cb428cd07fe587e2aee8b32bcc5c83
Lassi Piironen <lsipii@kapsi.fi>
2023-09-13 18:10:43 +0300

    Improve the tos-dataproduct descriptions

--- src/draft/Service/Terms/Agreement/Write.py
+++ src/draft/Service/Terms/Agreement/Write.py
@@ -2,67 +2,67 @@ from datetime import datetime
 from typing import Optional
 
 from definition_tooling.converter import CamelCaseModel, DataProductDefinition
-from pydantic import Field
+from pydantic import Field, HttpUrl
 
 
 class AgreementRequest(CamelCaseModel):
     accepted: bool = Field(
         ...,
         title="Accepted",
-        description="Whether the user has accepted the terms of service",
-        example=False,
+        description="Whether the service user has accepted the terms of service.",
+        example=True,
     )
     version: str = Field(
         ...,
         title="Version",
-        description="Version of the terms of service",
+        description="Version of the terms of service.",
         example="1.0",
     )
 
 
 class AgreementResponse(CamelCaseModel):
-    terms_of_service_url: str = Field(
+    terms_of_service_url: HttpUrl = Field(
         ...,
         title="Terms of service URL",
-        description="Link to the terms of service",
+        description="URL to the terms of service contents.",
         example="https://example.com/terms-of-service-v1.0",
     )
     description: str = Field(
         ...,
         title="Description",
         description="Description of the terms of service",
-        example="This is the terms of service for the example service",
+        example="This is the terms of service for the example service.",
     )
     version: str = Field(
         ...,
         title="Version",
-        description="Version of the terms of service",
+        description="Version of the terms of service.",
         example="1.0",
     )
     accepted: bool = Field(
         ...,
         title="Accepted",
-        description="Whether the user has accepted the terms of service",
+        description="Whether the service user has accepted the terms of service.",
         example=False,
     )
     accepted_at: Optional[datetime] = Field(
         None,
-        title="Datetime the terms of service was accepted at",
-        description="A datetime in RFC3339 date-time syntax",
-        example="2022-06-17T11:52:00Z",
+        title="Accepted at",
+        description="When the terms of service was accepted in RFC3339 date-time format. If the service user has not accepted the terms of service, this field is null.",
+        example="2023-09-13T11:52:00Z",
         nullable=True,
     )
     accepted_previous_version: bool = Field(
         ...,
         title="Accepted previous version",
-        description="Whether the user has accepted the previous version of the terms of service",
+        description="Whether the service user has accepted the previous version of the terms of service.",
         example=False,
     )
 
 
 DEFINITION = DataProductDefinition(
-    title="Update the user's terms of service agreement",
-    description="Data product for updating the user's terms of service agreement",
+    title="Write Terms of Service Agreement",
+    description="Update the service user's agreement to the terms of service.",
     request=AgreementRequest,
     response=AgreementResponse,
     requires_authorization=True,
