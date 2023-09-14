ddfa5ae677db641034b86e9422a2cef1f0fa9144
Lassi Piironen <lsipii@kapsi.fi>
2023-09-14 15:04:01 +0300

    Re-arrange the terms definition to a less confusing structure

--- src/Service/Terms/Agreement/Write_v0.1.py
+++ src/Service/Terms/Agreement/Write_v0.1.py
@@ -20,8 +20,8 @@ class AgreementRequest(CamelCaseModel):
     )
 
 
-class AgreementResponse(CamelCaseModel):
-    terms_of_service_url: HttpUrl = Field(
+class CurrentTerms(CamelCaseModel):
+    url: HttpUrl = Field(
         ...,
         title="Terms of service URL",
         description="URL to the terms of service contents.",
@@ -39,23 +39,32 @@ class AgreementResponse(CamelCaseModel):
         description="Version of the terms of service.",
         example="1.0",
     )
-    accepted: bool = Field(
+
+
+class AgreementResponse(CamelCaseModel):
+    current_terms: CurrentTerms = Field(
         ...,
-        title="Accepted",
-        description="Whether the service user has accepted the terms of service.",
+        title="Current terms",
+        description="The current terms of service.",
+    )
+    accepted_version: str = Field(
+        ...,
+        title="Accepted version",
+        description="The version the service user has accepted. If the service user has not accepted any version of the terms of service, this field is null.",
         example=False,
+        nullable=True,
     )
     accepted_at: Optional[datetime] = Field(
         None,
         title="Accepted at",
-        description="When the terms of service was accepted in RFC3339 date-time format. If the service user has not accepted the terms of service, this field is null.",
+        description="When the terms of service was accepted in RFC3339 date-time format. If the service user has not accepted any version of the terms of service, this field is null.",
         example="2023-09-13T11:52:00Z",
         nullable=True,
     )
-    accepted_previous_version: bool = Field(
+    has_accepted_latest: bool = Field(
         ...,
-        title="Accepted previous version",
-        description="Whether the service user has accepted the previous version of the terms of service.",
+        title="Has accepted the latest version",
+        description="Whether the service user has accepted the latest version of the terms of service.",
         example=False,
     )
 
