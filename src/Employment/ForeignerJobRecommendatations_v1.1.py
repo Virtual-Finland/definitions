aa985cf9908660da201822d1fff501e8702742e8
Joakim Nordling <joakim.nordling@ioxio.com>
2023-11-27 12:46:33 +0200

    More improvements to Employment/ForeignerJobRecommendations (#70)
    Allow the ESCO codes to be 1-3 digits without dots in addition to the 4 digits in the new 1.1.0 version, mark the 1.0.x as deprecated.

--- src/Employment/ForeignerJobRecommendatations_v1.1.py
+++ src/Employment/ForeignerJobRecommendatations_v1.1.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 from definition_tooling.converter import CamelCaseModel, DataProductDefinition
 from pydantic import Field, HttpUrl, constr
 
-EscoCode = constr(pattern=r"^[0-9]{4}(\.(?:([1-9]|[1-9][0-9]))){0,4}$")
+EscoCode = constr(pattern=r"^[0-9]{1,3}$|^[0-9]{4}(\.(?:([1-9]|[1-9][0-9]))){0,4}$")
 
 
 class FinnishMunicipality(str, Enum):
@@ -445,8 +445,7 @@ class ForeignerJobRecommendationsResponse(CamelCaseModel):
 
 
 DEFINITION = DataProductDefinition(
-    version="1.0.1",
-    deprecated=True,
+    version="1.1.0",
     title="Foreigner Job Recommendations",
     description="Returns the list of jobs recommended for the foreigner based on e.g. "
     "the citizenship area and previous occupations based on the European Standard "
