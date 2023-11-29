d2ff1c68abc17054d9f258db0286bab7a3fae6f6
Joakim Nordling <joakim.nordling@ioxio.com>
2023-11-29 09:34:09 +0200

    Fix typo in definition name and set version to appropriate level (#73)
    This copies the `Employment/ForeignerJobRecommendatations_v1.3` [sic] to `Employment/ForeignerJobRecommendations_v0.2`. This thus fixes the typo in the word recommendations. The old v1.3 is marked as deprecated. At same time the new, correctly named definition, gets the version number reset to pre 1.0 to show it's still in pretty early stage, as can be seen from the bunch of updates made to it this week. Once it's been tested out more we can increase the version to 1.0.
    
    The version number (0.2) might be a bit arbitrary, but this is on the other hand already iterated a few times and this would allow us if needed to also publish it on the DataFinland dataspace and there easily distinguish it from the one with the typo which is there called 0.1.

--- src/Employment/ForeignerJobRecommendations_v0.2.py
+++ src/Employment/ForeignerJobRecommendations_v0.2.py
@@ -445,8 +445,7 @@ class ForeignerJobRecommendationsResponse(CamelCaseModel):
 
 
 DEFINITION = DataProductDefinition(
-    version="1.3.0",
-    deprecated=True,
+    version="0.2.0",
     title="Foreigner Job Recommendations",
     description="Returns the list of jobs recommended for the foreigner based on e.g. "
     "the citizenship area and previous occupations based on the European Standard "
